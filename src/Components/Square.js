ebe7f039e7553ed513eb9891bb53a06970fd4131
andypike7 <tech@sym.ru>
2020-10-27 10:55:27 +0300

    Now Square is a function component

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,33 +1,15 @@
 import React from 'react';
 import './Square.scss';
 
-class Square extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     value: null,
-  //   };
-  // }
-
-  click = () => {
-    // alert('click');
-    console.log('*** CLICK on', this.props.value);
-    // this.setState({ value: 'X' });
-    this.props.onClick();
-  };
-
-  render() {
-    return (
-      <button
-        className="square"
-        // value={this.props.value}
-        onClick={this.click}
-      >
-        {this.props.value}
-        {/* {this.state.value} */}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button
+      className="square"
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
 }
 
 export default Square;
